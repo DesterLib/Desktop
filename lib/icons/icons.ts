a7af56c9ae9448b287a7d5a4d6fa2ded5c45c62a
AlkenD <desteralken@gmail.com>
2024-05-15 01:03:31 +0530

    fix: slider render issue in tauri

--- lib/icons/icons.ts
+++ lib/icons/icons.ts
@@ -4,6 +4,8 @@ import { ReactComponent as play } from "./assets/play.svg";
 import { ReactComponent as search } from "./assets/search.svg";
 import { ReactComponent as home } from "./assets/home.svg";
 import { ReactComponent as browse } from "./assets/browse.svg";
+import { ReactComponent as left } from "./assets/left.svg";
+import { ReactComponent as right } from "./assets/right.svg";
 
 export const icons = {
   bookmarks,
@@ -12,4 +14,6 @@ export const icons = {
   search,
   home,
   browse,
+  left,
+  right,
 };
