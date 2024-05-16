99408d169e192fc66103d13d058652171f4fed60
AlkenD <desteralken@gmail.com>
2024-05-17 00:22:36 +0530

    feat: add logo's and icons

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,7 @@
-import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Navbar from "@/lib/components/Navbar";
-import Sidebar from "@/lib/components/Sidebar";
-import Background from "@/lib/components/Background";
 
 const inter = Inter({ subsets: ["latin"] });
-
-export const metadata: Metadata = {
-  title: "Dester Desktop App",
-  description: "An intuvitive way to see all your content library at one place",
-};
 
 export default function RootLayout({
   children,
@@ -20,18 +11,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <div className="h-full w-full relative">
-          <Background />
-          <div className="flex h-full z-10 relative">
-            <Sidebar />
-            <div className="w-[calc(100%-68px)] h-screen">
-              <Navbar />
-              <div className="w-full h-[calc(100%-68px)] overflow-y-scroll rounded-lg pr-4">
-                {children}
-              </div>
-            </div>
-          </div>
-        </div>
+        {children}
       </body>
     </html>
   );
