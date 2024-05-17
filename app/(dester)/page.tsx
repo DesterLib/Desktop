3e058f89be21417b8bfd0151e36394138aeb7184
AlkenD <desteralken@gmail.com>
2024-05-17 18:18:34 +0530

    feat: working ffmpeg live transcoding demo

--- app/(dester)/page.tsx
+++ app/(dester)/page.tsx
@@ -1,17 +1,29 @@
 import { MainSlider } from "@/lib/components/MainSlider/MainSlider";
 import database from "@/db/db";
 import Slider from "@/lib/components/Slider";
+import VideoPlayer from "@/lib/components/VideoPlayer";
 
-export default function Home() {
+async function getData() {
+  const res = await fetch("http://localhost:3000/video");
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
+  return res.json();
+}
+
+const Home = async () => {
   return (
     <main className="w-full space-y-6">
-      <MainSlider data={database.mainSlider} />
+      <VideoPlayer />
+      {/* <MainSlider data={database.mainSlider} />
       <Slider
         title="Weekly Recommendation"
         data={database.weeklyRecommendations}
         gradiantText={true}
       />
-      <Slider title="Movies" data={database.movies} variant="v" />
+      <Slider title="Movies" data={database.movies} variant="v" /> */}
     </main>
   );
-}
+};
+
+export default Home;
