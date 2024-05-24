1b5b90d92e4ce51adafe35d76f87b82f4b6b8d42
AlkenD <desteralken@gmail.com>
2024-05-25 00:49:24 +0530

    fix: build error tw remove plugin import

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,14 +2,12 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./lib/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./public/splashscreen.html",
   ],
   theme: {
     extend: {},
   },
-  plugins: [require("tailwindcss-inner-border")],
+  plugins: [],
 };
 export default config;
